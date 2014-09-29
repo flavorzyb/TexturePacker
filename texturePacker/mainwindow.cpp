8c586c108c32838febd2323f4f9f2d4cd039f943
flavorzhu <haker-haker@163.com>
2014-09-29 17:33:38 +0800

    update

--- texturePacker/mainwindow.cpp
+++ texturePacker/mainwindow.cpp
@@ -1,11 +1,88 @@
 #include "mainwindow.h"
+#include <QVBoxLayout>
+#include <QtWidgets>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    initInputGroup();
+    initOutputGroup();
+    initOpGroup();
+    initUI();
+    initAction();
+    initMenu();
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+void MainWindow::initUI()
+{
+    setWindowTitle(tr("资源转换工具"));
+    setMinimumSize(800, 600);
+
+    QVBoxLayout * mainLayout = new QVBoxLayout();
+    mainLayout->addWidget(m_gbInputGroup);
+    //QWidget::setLayout: Attempting to set QLayout "" on MainWindow "", which already has a layout
+    setCentralWidget(m_gbInputGroup);
+    setLayout(mainLayout);
+}
+
+void MainWindow::initMenu()
+{
+    m_mAboutMe = menuBar()->addMenu("帮助(&H)");
+    m_mAboutMe->addAction(m_aAboutMe);
+}
+
+void MainWindow::initAction()
+{
+    m_aAboutMe = new QAction(tr("关于我(&A)"), this);
+    m_aAboutMe->setStatusTip(tr("关于软件的一些说明"));
+    connect(m_aAboutMe, SIGNAL(triggered()), this, SLOT(onAboutMeAction()));
+}
+
+void MainWindow::onAboutMeAction()
+{
+    printf("onAboutMeAction.......\n");
+}
+
+void MainWindow::initInputGroup()
+{
+    m_gbInputGroup = new QGroupBox();
+
+    QGridLayout * layout = new QGridLayout();
+
+    QLabel * lbInput = new QLabel();
+    lbInput->setText(tr("输入路径:"));
+    lbInput->setAlignment(Qt::AlignCenter | Qt::AlignLeft);
+
+    m_leInputDirPath = new QLineEdit();
+    m_leInputDirPath->setBaseSize(200, 40);
+    m_leInputDirPath->setText(tr("输入路径"));
+
+    m_pbInputDirPath = new QPushButton();
+    m_pbInputDirPath->setText(tr("设置输入目录"));
+    m_pbInputDirPath->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+
+    layout->setColumnStretch(0, 20);
+    layout->setColumnStretch(1, 40);
+    layout->setColumnStretch(2, 20);
+
+    layout->addWidget(lbInput, 0, 0);
+    layout->addWidget(m_leInputDirPath, 0, 1);
+    layout->addWidget(m_pbInputDirPath, 0, 2);
+
+    m_gbInputGroup->setLayout(layout);
+}
+
+void MainWindow::initOutputGroup()
+{
+
+}
+
+void MainWindow::initOpGroup()
+{
 }
--- texturePacker/mainwindow.h
+++ texturePacker/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtGui>
+#include <QtWidgets>
+#include <QGridLayout>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +13,27 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private:
+    void initUI();
+    void initMenu();
+    void initAction();
+    void initInputGroup();
+    void initOutputGroup();
+    void initOpGroup();
+private:
+    QAction     * m_aAboutMe;
+    QMenu       * m_mAboutMe;
+    QLineEdit   * m_leInputDirPath;
+    QLineEdit   * m_leOutputDirPath;
+    QPushButton * m_pbInputDirPath;
+    QPushButton * m_pbOutputDirPath;
+    QGroupBox   * m_gbInputGroup;
+    QGroupBox   * m_gbOutputGroup;
+    QGroupBox   * m_gbOpGroup;
+
+private slots:
+    void onAboutMeAction();
 };
 
 #endif // MAINWINDOW_H
