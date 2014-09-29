f5fbb61ce3e809a70cc5174a7dad0054cdd537a5
flavorzhu <haker-haker@163.com>
2014-09-29 23:29:24 +0800

    add font size

--- texturePacker/aboutme.cpp
+++ texturePacker/aboutme.cpp
@@ -11,34 +11,45 @@ AboutMe::AboutMe(QWidget *parent) :
 void AboutMe::initUI()
 {
     setWindowTitle(tr("关于我"));
-    setMinimumSize(600, 400);
+    setMinimumSize(400, 200);
+
+    QFont font;
+    font.setPointSize(22);
 
     QLabel *lbName = new QLabel();
     lbName->setText(tr("资源转换工具"));
+    lbName->setFont(font);
+    lbName->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 
     QString str =tr("作者:");
     str += Config::AUTHOR;
 
     QLabel *lbAuthor = new QLabel();
     lbAuthor->setText(str);
+    lbAuthor->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 
     str = tr("版本:");
     str += Config::VERSION;
 
     QLabel *lbVersion = new QLabel();
     lbVersion->setText(str);
+    lbVersion->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 
     str = tr("时间:");
     str += Config::DATE;
 
     QLabel *lbDate = new QLabel();
     lbDate->setText(str);
+    lbDate->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 
     QVBoxLayout * layout = new QVBoxLayout();
     layout->addWidget(lbName);
     layout->addWidget(lbAuthor);
     layout->addWidget(lbVersion);
     layout->addWidget(lbDate);
+    layout->setAlignment(Qt::AlignHCenter);
+    layout->setContentsMargins(10, 10, 10, 10);
+    layout->setSpacing(30);
 
     setLayout(layout);
 }
