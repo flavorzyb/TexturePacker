e3bb1de84b587ceccb45cf5a0be8aea6f13f6b87
flavorzhu <haker-haker@163.com>
2014-10-12 00:48:09 +0800

    add console mode

--- texturePacker/app/src/main.cpp
+++ texturePacker/app/src/main.cpp
@@ -1,17 +1,93 @@
 #include <QApplication>
 #include "include/mainwindow.h"
-#include "common/include/fileutils.h"
+#include "common/include/publisher.h"
+
+bool isConsole(int argc, char *argv[]);
+
+int runInConsole(int argc, char *argv[]);
+
+void print_usage();
+
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    if (isConsole(argc, argv))
+    {
+        return runInConsole(argc, argv);
+    }
+    else
+    {
+        QApplication a(argc, argv);
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }
+}
 
-//    for(int i = 0; i<argc; i++)
-//    {
-//        printf("argv[%d]=%s\n", i, argv[i]);
-//        FileUtils::writeFile("/Users/flavor/tmp/tp.dat", "ab+", argv[i], strlen(argv[i]));
-//    }
+bool isConsole(int argc, char *argv[])
+{
+    for(int i = 0; i<argc; i++)
+    {
+        QString param = argv[i];
 
-    MainWindow w;
-    w.show();
-    return a.exec();
+        if (param.toLower() == "-no-gui")
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int runInConsole(int argc, char *argv[])
+{
+    SettingsVO svo;
+
+    if (argc < 8)
+    {
+        print_usage();
+        return 0;
+    }
+
+    for (int i = 1; i < argc - 1; ++i)
+    {
+        if (strcmp("-i", argv[i])==0)
+        {
+            svo.setInputPath(argv[i+1]);
+        }
+        else if (strcmp("-o", argv[i])==0)
+        {
+            svo.setOutputPath(argv[i+1]);
+        }
+        else if (strcmp("-f", argv[i])==0)
+        {
+            if (strcmp("ios", argv[i+1]) == 0)
+            {
+                svo.setFormat(SettingsVO::IOS);
+            }
+            else if (strcmp("android", argv[i+1]) == 0)
+            {
+                svo.setFormat(SettingsVO::ANDROID);
+            }
+        }
+    }
+
+    if ((svo.getInputPath().length() < 1) || (svo.getOutputPath().length() < 1))
+    {
+        print_usage();
+        return 0;
+    }
+
+    Publisher pub(svo);
+    pub.publish();
+
+    return 0;
+}
+void print_usage()
+{
+    printf("error: error arguments\n");
+    printf("usage: texturePacker -no-gui -i /usr/local/input -o /usr/local/tmp/output -f ios \n");
+    printf("       -no-gui run application in console mode \n");
+    printf("       -i input directory path \n");
+    printf("       -o output directory path \n");
+    printf("       -f ios or android\n");
 }
--- texturePacker/texturePacker.pro
+++ texturePacker/texturePacker.pro
@@ -6,5 +6,4 @@
 TEMPLATE = subdirs
 
 SUBDIRS =   app \
-            cmd \
             tests
