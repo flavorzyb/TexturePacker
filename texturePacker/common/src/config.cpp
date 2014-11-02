b899c23caed635a3956b68cb01f660df8e1396a8
flavorzhu <haker-haker@163.com>
2014-11-02 13:32:18 +0800

    update config

--- texturePacker/common/src/config.cpp
+++ texturePacker/common/src/config.cpp
@@ -1,7 +1,7 @@
 #include "../include/config.h"
 
 const QString Config::AUTHOR = tr("朱彦斌");
-const QString Config::DATE = tr("2014-9-29");
+const QString Config::DATE = tr("2014-11-2");
 const QString Config::VERSION = tr("1.1.0");
 
 bool Config::m_isConsole = false;
