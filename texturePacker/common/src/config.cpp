cfa6821774c3ead034c0e38f5f26c77c914a0a49
flavorzhu <haker-haker@163.com>
2014-12-17 16:04:31 +0800

    update

--- texturePacker/common/src/config.cpp
+++ texturePacker/common/src/config.cpp
@@ -2,7 +2,7 @@
 
 const QString Config::AUTHOR = tr("朱彦斌");
 const QString Config::DATE = tr("2014-12-17");
-const QString Config::VERSION = tr("1.2.0");
+const QString Config::VERSION = tr("1.1.0");
 
 bool Config::m_isConsole = false;
 
